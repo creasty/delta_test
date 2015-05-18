79091b0d65d52f7d4225f4d7c68d163950c67256
Yuki Iwanaga <yuki@creasty.com>
2015-05-18 15:28:16 +0900

    Configure spec_helpers

--- spec/rails/spec/spec_helper.rb
+++ spec/rails/spec/spec_helper.rb
@@ -1,16 +1,50 @@
-require 'delta_test'
-require 'delta_test/spec_helpers'
+require 'rubygems'
+require 'spork'
 
-RSpec.configure do |config|
+Spork.prefork do
+  require File.expand_path('../../config/environment', __FILE__)
+  require 'rspec/rails'
 
-  config.include DeltaTest::SpecHelpers
+  require 'delta_test'
+  require 'delta_test/spec_helpers'
 
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  RSpec.configure do |config|
+
+    ENV['RAILS_ENV'] ||= 'test'
+
+    config.include DeltaTest::SpecHelpers
+    config.include FactoryGirl::Syntax::Methods
+
+    config.expect_with :rspec do |expectations|
+      expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    end
+
+    config.mock_with :rspec do |mocks|
+      mocks.verify_partial_doubles = true
+    end
+
+    config.before :each do |example|
+      DatabaseCleaner.strategy = if example.metadata[:js]
+          :truncation
+        else
+          :transaction
+        end
+
+      DatabaseCleaner.start
+    end
+
+    config.after :each do
+      DatabaseCleaner.clean
+    end
+
   end
+end
 
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
+Spork.each_run do
+  if Spork.using_spork?
+    ActionDispatch::Reloader.cleanup!
+    ActionDispatch::Reloader.prepare!
+    Rails.application.reload_routes!
+    FactoryGirl.reload
   end
-
 end
