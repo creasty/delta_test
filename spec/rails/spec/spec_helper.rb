25181ad8812cc707c69c5d93bb6eaa6f65f9228a
Yuki Iwanaga <yuki@creasty.com>
2015-05-18 16:02:43 +0900

    Setup capybara

--- spec/rails/spec/spec_helper.rb
+++ spec/rails/spec/spec_helper.rb
@@ -2,18 +2,28 @@ require 'rubygems'
 require 'spork'
 
 Spork.prefork do
+  ENV['RAILS_ENV'] ||= 'test'
+
   require File.expand_path('../../config/environment', __FILE__)
   require 'rspec/rails'
+  require 'capybara/rspec'
+  require 'capybara/poltergeist'
 
   require 'delta_test'
   require 'delta_test/spec_helpers'
 
   RSpec.configure do |config|
 
-    ENV['RAILS_ENV'] ||= 'test'
-
     config.include DeltaTest::SpecHelpers
     config.include FactoryGirl::Syntax::Methods
+
+    Capybara.javascript_driver = :poltergeist
+    Capybara.default_selector  = :css
+    Capybara.default_wait_time = 15
+
+    Capybara.register_driver :poltergeist do |app|
+      Capybara::Poltergeist::Driver.new(app, js_errors: true, timeout: 90)
+    end
 
     config.expect_with :rspec do |expectations|
       expectations.include_chain_clauses_in_custom_matcher_descriptions = true
