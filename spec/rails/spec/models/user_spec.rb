05c89a5f7cffa45869ffa579d83a398968d14e17
Yuki Iwanaga <yuki@creasty.com>
2015-05-18 16:12:23 +0900

    Tweak

--- spec/rails/spec/models/user_spec.rb
+++ spec/rails/spec/models/user_spec.rb
@@ -1,3 +1,20 @@
 describe User do
 
+  describe '.create' do
+
+    it 'should be a validation error without a name' do
+      user = build(:user, name: nil)
+      expect(user).not_to be_valid
+    end
+
+    it 'should create a user with a name' do
+      name = 'John Smith'
+      user = build(:user, name: name)
+      user.save
+      expect(user).to be_persisted
+      expect(user.name).to eq(name)
+    end
+
+  end
+
 end
