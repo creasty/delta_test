7bc2a814f7f44aaba2ae7385f8b11c9ad5de7d2f
Yuki Iwanaga <yuki@creasty.com>
2015-11-29 22:02:23 +0900

    Create exec command

--- lib/delta_test/cli/exec_command.rb
+++ lib/delta_test/cli/exec_command.rb
@@ -1,10 +1,93 @@
 require_relative 'command_base'
+require_relative '../stats'
+require_relative '../related_spec_list'
 
 module DeltaTest
   class CLI
     class ExecCommand < CommandBase
 
+      BUNDLE_EXEC = ['bundle', 'exec'].map(&:freeze).freeze
+      SPLITTER    = '--'.freeze
+
       def invoke!
+        if stats.base_commit
+          puts 'Base commit: %s' % [stats.base_commit]
+          puts
+        end
+
+        spec_files = nil
+        args = []
+
+        begin
+          unless profile_mode?
+            list.load_table!(stats.table_file_path)
+            list.retrive_changed_files!(stats.base_commit)
+
+            spec_files = list.related_spec_files.to_a
+
+            if spec_files.empty?
+              exit_with_message(0, 'Nothing to test')
+            end
+          end
+        rescue TableNotFoundError
+          # force profile mode cuz we don't have a table
+          @profile_mode = true
+        end
+
+        @args.map! { |arg| Shellwords.escape(arg) }
+
+        if (splitter = @args.index(SPLITTER))
+          files = @args.drop(splitter + 1)
+          @args = @args.take(splitter)
+
+          if files && files.any?
+            if spec_files
+              pattern = files.map { |file| Regexp.escape(file) }
+              pattern = '^(%s)' % pattern.join('|')
+              spec_files = spec_files.grep(pattern)
+            else
+              spec_files = files
+            end
+          end
+        end
+
+        if profile_mode?
+          args << ('%s=%s' % [VERBOSE_FLAG, true]) if DeltaTest.verbose?
+          args << ('%s=%s' % [ACTIVE_FLAG, true])
+        end
+
+        if spec_files
+          args.unshift('cat', '|')
+          args << 'xargs'
+        end
+
+        if bundler_enabled? && BUNDLE_EXEC != @args.take(2)
+          args += BUNDLE_EXEC
+        end
+
+        args += @args
+
+        $stdout.sync = true
+
+        exec_with_data(args.join(' '), spec_files)
+      end
+
+      def stats
+        @stats ||= Stats.new
+      end
+
+      def list
+        @list ||= RelatedSpecList.new
+      end
+
+      ###
+      # Whether run full test or not
+      #
+      # @return {Boolean}
+      ###
+      def profile_mode?
+        return @profile_mode if defined?(@profile_mode)
+        @profile_mode = !stats.base_commit
       end
 
     end
--- spec/lib/delta_test/cli/exec_command_spec.rb
+++ spec/lib/delta_test/cli/exec_command_spec.rb
@@ -2,4 +2,119 @@ require 'delta_test/cli/exec_command'
 
 describe DeltaTest::CLI::ExecCommand do
 
+  let(:args) { ['bundle', 'exec', 'rspec'] }
+  let(:command) { DeltaTest::CLI::ExecCommand.new(args) }
+
+  let(:table) do
+    {
+      'spec/foo_spec.rb' => ['lib/foo.rb']
+    }
+  end
+
+  let(:related_spec_files) do
+    [
+      'spec/foo_spec.rb',
+    ]
+  end
+
+  let(:base_commit) { '1111111111111111111111111111111111111111' }
+
+  before do
+    allow($stdout).to receive(:puts).and_return(nil)
+    allow($stderr).to receive(:puts).and_return(nil)
+
+    allow(command.list).to receive(:load_table!).and_return(nil)
+    allow(command.list).to receive(:table).and_return(table)
+    allow(command.list).to receive(:retrive_changed_files!).and_return(nil)
+    allow(command.list).to receive(:related_spec_files).and_return(related_spec_files)
+
+    allow(command.stats).to receive(:base_commit).and_return(base_commit)
+  end
+
+  describe '#profile_mode?' do
+
+    it 'should return false if a base commit exists' do
+      expect(command.profile_mode?).to be(false)
+    end
+
+    it 'should return true if a base commit does not exist' do
+      allow(command.stats).to receive(:base_commit).and_return(nil)
+      expect(command.profile_mode?).to be(true)
+    end
+
+    it 'should be able to override by an instance variable' do
+      expect(command.profile_mode?).to be(false)
+      command.instance_variable_set(:@profile_mode, true)
+      expect(command.profile_mode?).to be(true)
+    end
+
+  end
+
+  describe '#invoke!' do
+
+    before do
+      allow(command).to receive(:exec_with_data).and_return(nil)
+    end
+
+    context 'Full tests' do
+
+      before do
+        allow(command).to receive(:profile_mode?).and_return(true)
+      end
+
+      it 'should run script with a flag' do
+        expect(command.list).not_to receive(:related_spec_files)
+
+        _args = ['%s=%s' % [DeltaTest::ACTIVE_FLAG, true], *args].join(' ')
+        expect(command).to receive(:exec_with_data).with(_args, nil)
+
+        expect {
+          command.invoke!
+        }.not_to raise_error
+      end
+
+    end
+
+    context 'Partial tests' do
+
+      before do
+        allow(command).to receive(:profile_mode?).and_return(false)
+      end
+
+      context 'Any related files' do
+
+        it 'should run script with related spec files' do
+          expect(command.list).to receive(:related_spec_files)
+
+          _args = ['cat', '|', 'xargs', *args].join(' ')
+          expect(command).to receive(:exec_with_data).with(_args, related_spec_files)
+
+          expect {
+            command.invoke!
+          }.not_to raise_error
+        end
+
+      end
+
+      context 'No related files' do
+
+        let(:related_spec_files) { [] }
+
+        it 'should not run script and exit with a message' do
+          expect(command.list).to receive(:related_spec_files)
+
+          expect {
+            begin
+              command.invoke!
+            rescue SystemExit => e
+              expect(e.status).to eq(0)
+            end
+          }.to output(/Nothing/).to_stdout
+        end
+
+      end
+
+    end
+
+  end
 end
