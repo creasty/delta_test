8456b0b2f5ba418c05f19561ced67d869bb528c7
Yuki Iwanaga <yuki@creasty.com>
2015-11-30 00:52:56 +0900

    Write specs for StatsCleanCommand

--- spec/lib/delta_test/cli/stats_clean_command_spec.rb
+++ spec/lib/delta_test/cli/stats_clean_command_spec.rb
@@ -2,4 +2,29 @@ require 'delta_test/cli/stats_clean_command'
 
 describe DeltaTest::CLI::StatsCleanCommand do
 
+  let(:tmp_dir) { DeltaTest.config.tmp_table_file.parent }
+  let(:command) { DeltaTest::CLI::StatsCleanCommand.new([]) }
+
+  describe '#invoke!' do
+
+    it 'should not raise any error if a tmporary directory does not exist' do
+      expect(File.directory?(tmp_dir)).to be(false)
+      expect {
+        command.invoke!
+      }.not_to raise_error
+    end
+
+    it 'should delete all files in the temporary directory' do
+      FakeFS::FileSystem.add(tmp_dir.join('foo'), FakeFS::FakeFile.new)
+      FakeFS::FileSystem.add(tmp_dir.join('bar'), FakeFS::FakeFile.new)
+
+      expect(File.directory?(tmp_dir)).to be(true)
+      expect {
+        command.invoke!
+      }.not_to raise_error
+      expect(File.directory?(tmp_dir)).to be(false)
+    end
+
+  end
+
 end
