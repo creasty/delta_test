a72688506d64c1b7bb40d59a61bc9259cbd8915e
Yuki Iwanaga <yuki@creasty.com>
2015-11-29 17:51:06 +0900

    Tweak stats

--- lib/delta_test/stats.rb
+++ lib/delta_test/stats.rb
@@ -6,28 +6,33 @@ module DeltaTest
     attr_reader :base_git
     attr_reader :stats_git
 
+    TABLE_FILENAME = 'table.marshal'
+
     def initialize
       @base_git  = Git.new(DeltaTest.config.base_path)
       @stats_git = Git.new(DeltaTest.config.stats_path)
     end
 
-    def find_base_commit
+    def base_commit
+      return @base_commit if defined?(@base_commit)
+
       indexes = @stats_git.ls_files
         .map { |f| f.sub('/', '') }
         .to_set
 
-      @base_git.ls_hashes(DeltaTest.config.stats_life)
+      @base_commit = @base_git.ls_hashes(DeltaTest.config.stats_life)
         .find { |h| indexes.include?(h) }
     end
 
-    def find_commit_dir
-      _base_commit_dir = find_base_commit
-      return unless _base_commit_dir
-      self.class.find_commit_dir(_base_commit_dir)
+    def commit_dir
+      return unless base_commit
+      return @commit_dir if defined?(@commit_dir)
+
+      @commit_dir = DeltaTest.config.stats_path.join(*base_commit.unpack('A2A*'))
     end
 
-    def self.find_commit_dir(commit)
-      DeltaTest.config.stats_path.join(*commit.unpack('A2A*'))
+    def table_file_path
+      commit_dir && commit_dir.join(TABLE_FILENAME)
     end
 
   end
--- spec/lib/delta_test/stats_spec.rb
+++ spec/lib/delta_test/stats_spec.rb
@@ -15,48 +15,71 @@ describe DeltaTest::Stats do
 
   end
 
-  describe '::find_commit_dir' do
+  let(:stats) { DeltaTest::Stats.new }
 
-    let(:commit_hash) { '0000000000000000000000000000000000000000' }
-    let(:commit_dir)  { DeltaTest.config.stats_path.join('00/00000000000000000000000000000000000000') }
+  let(:commit_hashes) do
+    [
+      '4444444444444444444444444444444444444444',
+      '3333333333333333333333333333333333333333',
+      '2222222222222222222222222222222222222222',
+      '1111111111111111111111111111111111111111',
+      '0000000000000000000000000000000000000000',
+    ]
+  end
 
-    it 'should return a file for the commit hash' do
-      expect(DeltaTest::Stats.find_commit_dir(commit_hash)).to eq(commit_dir)
+  let(:commit_dirs) do
+    [
+      '11/11111111111111111111111111111111111111',
+      '33/33333333333333333333333333333333333333',
+    ]
+  end
+
+  before do
+    allow_any_instance_of(DeltaTest::Git).to receive(:ls_hashes)
+      .with(DeltaTest.config.stats_life)
+      .and_return(commit_hashes)
+    allow_any_instance_of(DeltaTest::Git).to receive(:ls_files).and_return([])
+  end
+
+  describe '#base_commit' do
+
+    it 'should return a base commit if exists' do
+      allow_any_instance_of(DeltaTest::Git).to receive(:ls_files).and_return(commit_dirs)
+      expect(stats.base_commit).to eq('3333333333333333333333333333333333333333')
+    end
+
+    it 'should return nil if not exists' do
+      expect(stats.base_commit).to be_nil
     end
 
   end
 
-  describe '#find_base_commit' do
+  describe '#commit_dir' do
 
-    let(:stats) { DeltaTest::Stats.new }
+    let(:commit_dir) { DeltaTest.config.stats_path.join('33/33333333333333333333333333333333333333') }
 
-    let(:commit_hashes) do
-      [
-        '4444444444444444444444444444444444444444',
-        '3333333333333333333333333333333333333333',
-        '2222222222222222222222222222222222222222',
-        '1111111111111111111111111111111111111111',
-        '0000000000000000000000000000000000000000',
-      ]
+    it 'should return a file for the commit hash if base_commit exists' do
+      allow_any_instance_of(DeltaTest::Git).to receive(:ls_files).and_return(commit_dirs)
+      expect(stats.commit_dir).to eq(commit_dir)
     end
 
-    let(:commits) do
-      [
-        '11/11111111111111111111111111111111111111',
-        '33/33333333333333333333333333333333333333',
-      ]
+    it 'should return nil if base_commit does not exist' do
+      expect(stats.commit_dir).to be_nil
     end
 
-    before do
-      allow_any_instance_of(DeltaTest::Git).to receive(:ls_hashes)
-        .with(DeltaTest.config.stats_life)
-        .and_return(commit_hashes)
+  end
 
-      allow_any_instance_of(DeltaTest::Git).to receive(:ls_files).and_return(commits)
+  describe '#table_file_path' do
+
+    let(:table_file_path) { DeltaTest.config.stats_path.join('33/33333333333333333333333333333333333333/table.marshal') }
+
+    it 'should return a path of table file' do
+      allow_any_instance_of(DeltaTest::Git).to receive(:ls_files).and_return(commit_dirs)
+      expect(stats.table_file_path).to eq(table_file_path)
     end
 
-    it 'should return the newest commit in the commits' do
-      expect(stats.find_base_commit).to eq('3333333333333333333333333333333333333333')
+    it 'should return nil if base_commit does not exist' do
+      expect(stats.table_file_path).to be_nil
     end
 
   end
