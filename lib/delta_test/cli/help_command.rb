7555a107b688b2cb13611bef7b5b0af537415777
Yuki Iwanaga <yuki@creasty.com>
2015-11-29 18:24:35 +0900

    Fix help text

--- lib/delta_test/cli/help_command.rb
+++ lib/delta_test/cli/help_command.rb
@@ -11,15 +11,16 @@ options:
 --verbose      Print more output.
 
 commands:
-list           List related spec files for changes.
-
-table          Show dependencies table.
-
 exec [--force-run] <script> -- <files...>
                Execute test script using delta_test.
                --force-run to force DeltaTest to run full test cases.
 
-save           Clean up tables and caches.
+specs          List related spec files for changes.
+
+
+stats:show     Show dependencies table.
+
+stats:save     Clean up tables and caches.
 
 version        Show version.
 
